bb22e62cba4e23f6388b4281474626caf335e02e
Username98673 <szymon.kitel@gmail.com>
2024-10-09 15:16:22 +0200

    wyodrebnienie jezykow

--- app/src/main/java/com/example/flagi/MainActivity.java
+++ app/src/main/java/com/example/flagi/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.flagi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int licznikZlych=0;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textViewPolecenie);
+    }
+
+    public void zlaOdpowiedz(View view) {
+
+        Toast.makeText(this, R.string.podsumowanie, Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void dobraOdp(View view) {
+        view.setVisibility(View.INVISIBLE);
+        licznikZlych++;
+        if (licznikZlych==4){
+            textView.setText("To jest flaga Polski");
+        }
+
     }
 }
